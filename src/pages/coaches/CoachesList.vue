701688f6069dd41efa02f3ff8608beb790fd409c
zoro-2002 <premambre80@gmail.com>
2022-07-10 00:21:27 +0530

    add : added the coachItem component and integrated with the coach list component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,40 @@
 <template>
     <section class="coaches_list">
-    <div class="controls">
-        <button>Refresh</button>
-    </div>
-    <ul v-if="hasCoaches">
-        <li v-for="coach in coaches" :key="coach.id">{{coach.firstName}}</li>
-    </ul>
-    <h3 v-else>No Coaches Found</h3>
-    <router-link to="/register">Register as Coach</router-link>
+        <div class="controls">
+            <button>Refresh</button>
+        </div>
+        <ul v-if="hasCoaches">
+            <coach-item v-for="coach in coaches" :key="coach.id" :id="coach.id"
+                :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
+                :areas="coach.areas"></coach-item>
+        </ul>
+        <h3 v-else>No Coaches Found</h3>
+        <router-link to="/register">Register as Coach</router-link>
     </section>
 </template>
 
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
 import { mapGetters } from "vuex";
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
-        ...mapGetters('coaches', ['coaches','hasCoaches']),
+        ...mapGetters('coaches', ['coaches', 'hasCoaches']),
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
