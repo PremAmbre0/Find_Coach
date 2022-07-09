fc12b7b4c99df9ac5e8ca043870f136eaccf8a1c
zoro-2002 <premambre80@gmail.com>
2022-07-09 23:30:46 +0530

    add : add the basic file structure nad the route of the project

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,11 @@
+<template>
+<section class="coaches_list_co">
+<section>
+    FIlTER
+</section>
+<section>
+    COACHES LIST
+</section>
+</section>
+</template>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import CoachDeatails from "../pages/coaches/CoachDetails.vue"
+import CoachesList from "../pages/coaches/CoachesList.vue"
+import CoachRegistration from "../pages/coaches/CoachRegistration.vue"
+import ContactCoach from "../pages/requests/ContactCoach.vue"
+import RequestReciecved from "../pages/requests/RequestRecieved.vue"
+import NotFound from "../pages/NotFound.vue"
 
 Vue.use(VueRouter);
 
 const routes = [
+  { path: '/', redirect: '/coaches' },
+  { path: '/coaches', component: CoachesList },
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/coaches/:id',
+    component: CoachDeatails,
+    children: [
+      { path: 'contact', component:ContactCoach }
+    ]
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  { path: '/register', component: CoachRegistration },
+  { path: '/requests', component: RequestReciecved },
+  { path: '/:not Found(.*)', component: NotFound }
 ];
 
 const router = new VueRouter({
