8e8af0eb32dda0a681318a39dea3bb94fb3c2771
zoro-2002 <premambre80@gmail.com>
2022-07-10 20:37:29 +0530

    add : addec the coach list component and integrated in the coach list component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,44 +1,76 @@
 <template>
     <section class="coaches_list">
+        <section>
+            <coach-filter @change-filter="setFilter"></coach-filter>
+        </section>
         <custom-card>
-        <div class="controls">
-            <custom-button mnod="outline">Refresh</custom-button>
-            <custom-button link to="/register">Register as Coach</custom-button>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item v-for="coach in coaches" :key="coach.id" :id="coach.id"
-                :first-name="coach.firstName" :last-name="coach.lastName" :rate="coach.hourlyRate"
-                :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else>No Coaches Found</h3>
+            <div class="controls">
+                <custom-button mnod="outline">Refresh</custom-button>
+                <custom-button link to="/register">Register as Coach</custom-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>No Coaches Found</h3>
         </custom-card>
     </section>
 </template>
 
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import CoachFilter from "../../components/coaches/CoachFilter.vue";
 import { mapGetters } from "vuex";
-import CustomButton from "../../components/CustomComponnets/CustomButton.vue";
 export default {
     components: {
-    CoachItem,
-    CustomButton
-},
+        CoachItem,
+        CoachFilter,
+    },
+    data() {
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true
+            }
+        }
+    },
     computed: {
         ...mapGetters('coaches', ['coaches', 'hasCoaches']),
+        filteredCoaches() {
+            const coaches = this.coaches;
+            return coaches.filter((coach) => {
+                if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+                    return true;
+                }
+                if (this.activeFilters.backend && coach.areas.includes('backend')) {
+                    return true;
+                }
+                if (this.activeFilters.career && coach.areas.includes('career')) {
+                    return true;
+                }
+                return false;
+            });
+        },
+    },
+    methods: {
+        setFilter(filters) {
+            this.activeFilters = filters
+        }
     }
 }
+
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+    list-style: none;
+    margin: 0;
+    padding: 0;
 }
 
 .controls {
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
 </style>
