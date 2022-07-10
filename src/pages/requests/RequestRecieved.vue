704d76c704adaec9a66298a6f28ab369842150d0
zoro-2002 <premambre80@gmail.com>
2022-07-11 01:05:18 +0530

    add : added the RequestRecieved and RequestItems componets
    feat: added the getters requests and hasRequests to the requestslist module of the store

---
+++ src/pages/requests/RequestRecieved.vue
@@ -0,0 +1,45 @@
+<template>
+    <section>
+        <custom-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+            <ul v-if="hasRequests">
+                <request-items v-for="req in requests" :key="req.id" :email="req.userEmail" :message="req.message">
+                </request-items>
+            </ul>
+            <h3 v-else>You haven't received any requests yet!</h3>
+        </custom-card>
+    </section>
+</template>
+
+<script>
+import RequestItems from "../../components/requests/RequestItems.vue"
+import { mapGetters } from "vuex";
+
+export default {
+    components: {
+        RequestItems
+    },
+    computed: {
+        ...mapGetters('requests', ['requests', 'hasRequests'])
+    }
+}
+
+</script>
+<style scoped>
+header {
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
+</style>
--- src/store/modules/requestsList.js
+++ src/store/modules/requestsList.js
@@ -3,7 +3,12 @@ const state = {
 
 }
 const getters = {
-
+    requests(state){
+        return state.requests
+    },
+    hasRequests(state){
+        return state.requests && state.requests.length > 0
+    }
 }
 const mutations = {
     addRequest(state, payload) {
