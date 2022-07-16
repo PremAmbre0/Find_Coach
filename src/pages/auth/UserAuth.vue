baa926fbe18970beab6e42f15e963593284f218f
zoro-2002 <premambre80@gmail.com>
2022-07-16 17:05:39 +0530

    add : added form validation for password and email with regex

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -17,6 +17,7 @@
                 <div class="form-control">
                     <label for="password">Password</label>
                     <input type="password" id="password" v-model.trim="password">
+                    <div :class="passwordDisplayClass">{{ strengthTextDisplay }}</div>
                 </div>
                 <p v-if="!formIsValid">Enter valid email and pasword (Password should atleast be of 7 characters)
                 </p>
@@ -57,13 +58,74 @@ export default {
             } else {
                 return 'Login Instead'
             }
+        },
+        passwordStrength() {
+            let strength = 0;
+            if (this.password.length < 6) {
+                return -1
+            }
+            if (this.password.length > 6) {
+                strength += 1
+                if (/[a-z]+/g.test(this.password)) {
+                    strength += 1
+                }
+                if (/[A-Z]+/g.test(this.password)) {
+                    strength += 1
+                }
+                if (/[0-9]+/g.test(this.password)) {
+                    strength += 1
+                }
+                if (/[!@#$%^&*]+/g.test(this.password)) {
+                    strength += 1
+                }
+            }
+            return strength
+        },
+        strengthTextDisplay() {
+            let text = ''
+            if (this.passwordStrength == 0) {
+                text = 'please enter the password'
+            }
+            if (this.passwordStrength == 1) {
+                text = 'very poor'
+            }
+            if (this.passwordStrength == 2) {
+                text = 'poor'
+            }
+            if (this.passwordStrength == 3) {
+                text = 'moderate'
+            }
+            if (this.passwordStrength == 4) {
+                text = 'strong'
+            }
+            if (this.passwordStrength == 5) {
+                text = 'very strong'
+            }
+            if (this.passwordStrength == -1) {
+                text = 'password should atleast be of 7 characters'
+            }
+            return text
+        },
+        passwordDisplayClass() {
+            let className = ''
+            if (this.strengthTextDisplay == 'please enter the password' || this.strengthTextDisplay == 'very poor' || this.strengthTextDisplay == 'poor' || this.strengthTextDisplay == 'password should atleast be of 7 characters') {
+                className = 'poor'
+            }
+            if (this.strengthTextDisplay == 'moderate') {
+                className = 'moderate'
+            } if (this.strengthTextDisplay == 'strong' || this.strengthTextDisplay == 'very strong') {
+                className = 'strong'
+            }
+            return className
         }
     },
     methods: {
         ...mapActions('auth', ['signUp', 'logIn']),
         async submitForm() {
-            // console.log(this.userAuthMode);
-            if (this.email == "" || !this.email.includes('@') || this.password.length < 6) {
+            let emailValidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/.test(this.email)
+            console.log(this.emailValidation)
+            if (!emailValidation || this.passwordStrength === 0 || this.password.length === 0) {
+                console.log(this.formIsValid);
                 this.formIsValid = false
                 return;
             }
@@ -97,7 +159,7 @@ export default {
         handleError() {
             this.error = null;
             // console.log(this.error)
-        }
+        },
     }
 }
 
@@ -133,5 +195,20 @@ textarea:focus {
     border-color: #3d008d;
     background-color: #faf6ff;
     outline: none;
+}
+
+.poor {
+    color: red;
+    font-weight: bold;
+}
+
+.moderate {
+    color: yellowgreen;
+    font-weight: bold;
+}
+
+.strong {
+    color: rgb(75, 196, 75);
+    font-weight: bold;
 }
 </style>
